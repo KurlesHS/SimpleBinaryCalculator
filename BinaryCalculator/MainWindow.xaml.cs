3416ccc6406b5577064ca7f2d8dd72b690316fe0
KurlesHS <kahshs@gmail.com>
2014-02-04 00:13:31 +0400

    added text input block

--- BinaryCalculator/MainWindow.xaml.cs
+++ BinaryCalculator/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Windows;
 using System.Windows.Controls;
@@ -28,16 +29,19 @@ namespace BinaryCalculator
     public partial class MainWindow
     {
         private readonly List<BinarySelector> _listOfSelectors;
+        private string _lastNumber;
+        private bool _needToRecalculateNum;
 
         public MainWindow() {
             InitializeComponent();
             _listOfSelectors = new List<BinarySelector>();
+            _needToRecalculateNum = true;
             for (var i = 31; i >= 0; --i) {
                 var bs = new BinarySelector(i);
                 bs.CheckBox.Checked += RecalculateNumber;
                 bs.CheckBox.Unchecked += RecalculateNumber;
                 _listOfSelectors.Add(bs);
-                stackPanel.Children.Add(bs);
+                StackPanel.Children.Add(bs);
             }
             RecalculateNumber();
         }
@@ -50,15 +54,50 @@ namespace BinaryCalculator
                     number |= 0x01;
                 }
             }
-            hexString.Content = "0x" + number.ToString("X8");
+            _lastNumber = "0x" + number.ToString("X8");
+            HexString.Content = _lastNumber;
+            TexBlock.Text = HexString.Content.ToString();
         }
 
         private void RecalculateNumber(object sender, RoutedEventArgs e) {
-            RecalculateNumber();
+            if (_needToRecalculateNum)
+                RecalculateNumber();
         }
 
         private void buttonCopyToContext_Click(object sender, RoutedEventArgs e) {
-            Clipboard.SetText(hexString.Content.ToString());
+            Clipboard.SetText(HexString.Content.ToString());
+        }
+
+        private void TexBlock_OnTextChanged(object sender, TextChangedEventArgs e) {
+            int hexNumber;
+            var text = TexBlock.Text.Trim(' ').ToLower();
+            var isHexValue = false;
+            var numStyles = NumberStyles.Integer;
+            if (text.Length > 2 && text.Substring(0, 2) == "0x") {
+                text = text.Substring(2);
+                isHexValue = true;
+                numStyles = NumberStyles.HexNumber;
+            }
+                
+            try {
+                int.TryParse(text, numStyles, CultureInfo.CurrentCulture, out hexNumber);
+            } catch (ArgumentException) {
+                TexBlock.Text = _lastNumber;
+                return;
+            }
+            _lastNumber = isHexValue ? hexNumber.ToString("X") : hexNumber.ToString(CultureInfo.InvariantCulture);
+            HexString.Content = "0x" + hexNumber.ToString("X8");
+            var bit = 0x01;
+            _needToRecalculateNum = false;
+            if (_listOfSelectors != null) {
+                for (var idx = _listOfSelectors.Count - 1; idx >= 0; --idx)
+                {
+                    _listOfSelectors[idx].CheckBox.IsChecked = (hexNumber & bit) != 0;
+                    bit <<= 1;
+                }    
+            }
+            _needToRecalculateNum = true;
+
         }
     }
 }
